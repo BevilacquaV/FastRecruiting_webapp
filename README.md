848151be8ee1bfaca045831497d52692c3e7c36d
Antonio De Piano <depianoantonio@gmail.com>
2017-12-08 11:20:30 +0100

    Fist Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FastRecruiting_web
---
+++ src/app/layout/selected-candidates/selected-candidates.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SelectedCandidatesRoutingModule } from './selected-candidates-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SelectedCandidatesRoutingModule
+  ],
+  declarations: []
+})
+export class SelectedCandidatesModule { }
