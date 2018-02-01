6aed1f772e56611871fb0f34bcb80d5182d47cc9
Antonio De Piano <depianoantonio@gmail.com>
2018-02-01 17:19:21 +0100

    dettaglio annuncio

--- src/app/layout/dettaglio-annuncio/dettaglio-annuncio.component.ts
+++ src/app/layout/dettaglio-annuncio/dettaglio-annuncio.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import { AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
 import {Joboffer} from '../dashboard/joboffer';
 import {FirebaseApp} from 'angularfire2';
+import {Skills} from '../public-ad/skills';
 
 @Component({
   selector: 'app-dettaglio-annuncio',
@@ -13,64 +14,148 @@ import {FirebaseApp} from 'angularfire2';
 })
 export class DettaglioAnnuncioComponent implements OnInit, OnDestroy {
 
-    private sub: any;
     database;
-    log: string;
-    flag: boolean;
+    flag;
+    i;
+    j;
+    n;
     job: Joboffer;
-    d;
-    key: String;
-    ob: Joboffer;
-    ref;
-    name: String;
-        fullname: String;
-    constructor(private db: AngularFireDatabase, private route: ActivatedRoute) {
+    public list: Array<any> = [];
+    public listskill: Array<any> = [];
+    key;
+    sub;
+    databaseskill;
+    public skill: Skills = {nome: ''};
 
+    constructor(private db: AngularFireDatabase, private route: ActivatedRoute, private router: Router) {
 
-        this.job = {titolo: '', luogodilavoro: '',
-            skill: '', annuncio: '', titolodistudio: '', key: ''};
+        this.i = 0;
+        this.j = 0;
+        this.n = 0;
 
+        this.database = this.db.list('/skill/');
+        this.database.valueChanges().forEach(el => {
+            el.forEach(element => {
+                this.skill = new Skills(element.nome);
+                this.listskill.push(this.skill);
+            });
+
+        });
         this.sub = this.route.params.subscribe(params => {
             this.key = params['key'];
             console.log('leggo: ', params['key']);
         });
+        this.job = {titolo: '', luogodilavoro: '', skill: '', annuncio: '', titolodistudio: '', key: ''};
 
-        /*
-        this.ref = dat.database().ref('/offertedilavoro/');
-        console.log(this.ref.database.titolodistudio);
-        this.ref.once('value')
-            .then(function(snapshot) {
-                this.name = snapshot.child('-L2UpPY2VQxcrXc9fZUK').val();
-                this.fullname = snapshot.child(this.key + '/titolo').val();
-                console.log('this.name: ', this.name, ' this.fullname: ', this.fullname);
-            });
-
-*/
         this.database = this.db.list('/offertedilavoro/');
 
-        this.db.list('/offertedilavoro').snapshotChanges().map(actions => {
+        this.db.list('/offertedilavoro', ref => ref.orderByChild('titolo')).snapshotChanges().map(actions => {
             return actions.map(action => ({ key: action.key, ...action.payload.val() }));
         }).subscribe(items => {
-            items.forEach(it => {
-
-                if (this.key !== it.key) {
-                } else {
-                    this.ob = new Joboffer(it.titolo, it.luogodilavoro, it.skill, it.annuncio,
-                        it.titolodistudio, it.key);
-                    this.job.titolo = it.titolo;
-                    this.job.luogodilavoro = it.luogodilavoro;
-                    this.job.skill = it.skill;
-                    this.job.annuncio = it.annuncio;
-                    this.job.titolo = it.titolo;
-                    this.job.titolodistudio = it.titolodistudio;
-
+            items.forEach( it => {
+                if (this.key === it.key ) {
+                    this.job = {titolo: it.titolo, luogodilavoro: it.luogodilavoro, skill: it.skill, annuncio: it.annuncio,
+                        titolodistudio: it.titolodistudio, key:  it.key};
+                    this.skill = new Skills(it.skill);
+                    this.list.push(this.job);
+                    console.log('Annuncio: ', it.annuncio, ' Luogo di lavoro: ', it.luogodilavoro,
+                        ' titolo di studio: ', it.titolodistudio, ' key: ', it.key);
                 }
             });
         });
-}
+    }
 
-ngOnInit() {
-}
+
+
+
+    ngOnInit() {}
+
+    onUpdateOfferta() {
+        this.database = this.db.list('/offertedilavoro/');
+        this.onUpdateTitolo();
+        this.onUpdateAnnuncio();
+        this.onUpdateTitolodistudio();
+        this.onUpdateLuogodilavoro();
+        this.onAggiornaSkills();
+
+    }
+
+    onUpdateTitolo() {
+        this.database.update(this.key, {titolo: this.job.titolo});
+        console.log('titolo cambiato;');
+    }
+    onUpdateAnnuncio() {
+        this.database.update(this.key, {annuncio: this.job.annuncio});
+        console.log('titolo Annuncio;');
+    }
+    onUpdateTitolodistudio() {
+        this.database.update(this.key, {titolodistudio: this.job.titolodistudio});
+        console.log('titolo titolodistudio;');
+    }
+    onUpdateLuogodilavoro() {
+        this.database.update(this.key, {luogodilavoro: this.job.luogodilavoro});
+        console.log('titolo luogodilavoro;');
+    }
+    onAggiornaSkills() {
+        this.database.update(this.key, {skill: this.job.skill});
+        console.log('skill cambiate)');
+    }
+    onDeleteAd() {
+        this.db.list('/offertedilavoro').remove(this.key);
+        this.router.navigateByUrl('/dashboard');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    onControlSkill(skill: string) {
+        this.database = this.db.list('/skill/');
+        this.flag = false;
+        this.i = 0;
+        if (skill.length === 0) {
+            this.i++;
+            alert(' campo vuoto');
+        }
+        this.database.valueChanges().forEach(el => {
+            el.forEach(element => {
+                if ( element.nome  === skill) {
+                    this.flag = true;
+                    this.i++;
+                }
+
+            });
+            if (this.flag === false && this.i === 0 ) {
+                this.onAddSkill(skill);
+            } else {
+                console.log('skill esistente'); }
+        });
+
+    }
+    onAddSkill(skill: string) {
+        this.database = this.db.list('/skill');
+        const saveData = {nome: skill};
+        this.database.push( saveData );
+        this.onUpdateSkill();
+
+    }
+    onUpdateSkill() {
+        this.n = this.listskill.length;
+        for (this.j = 0; this.j <= this.n ; this.j++) {
+            this.listskill.pop();
+        }
+    }
 
 ngOnDestroy() {
  this.sub.unsubscribe();
